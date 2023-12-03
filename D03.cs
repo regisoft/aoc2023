52ef9d9f82ab1ef73ae808302a8afaed3bf86e52
ivo <ivo.marti@edorex.ch>
2023-12-03 11:27:17 +0100

    D03_1

---
+++ D03.cs
@@ -0,0 +1,83 @@
+namespace aoc2023;
+
+[TestClass]
+public class D03
+{
+    [TestMethod]
+    public void P1()
+    {
+        var rslt = Calc1($"_{nameof(D03)}_1_sample.txt");
+        Assert.AreEqual(4361, rslt);
+
+        rslt = Calc1($"_{nameof(D03)}.txt");
+        Assert.AreEqual(535351, rslt);
+    }
+
+    public static int Calc1(string inputName)
+    {
+        var input = File.ReadLines(inputName);
+        var matrix = input.Select(l => l.ToCharArray()).ToArray();
+
+        int sum = 0;
+
+        for (int y = 0; y < matrix.Length; y++)
+        {
+            int start = -1;
+
+            for (int x = 0; x < matrix[y].Length; x++)
+            {
+                var current = matrix[y][x];
+                if (char.IsDigit(current))
+                {
+                    if (start == -1) start = x;
+                }
+                else
+                {
+                    if (start != -1)
+                    {
+                        var end = x-1;
+                        if (HasSymbolAround(y, start, end))
+                        {
+                            string substring = string.Empty;
+                            for (int idx = start; idx <= end; idx++)
+                            {
+                                substring += matrix[y][idx];
+                            }
+                            sum += Convert.ToInt32(substring);
+                        }
+                        start = -1;
+                    }
+                }
+            }
+        }
+
+        bool HasSymbolAround(int yStart, int xStart, int xEnd)
+        {
+            for (int y = yStart - 1; y <= yStart + 1; y++)
+            {
+                for (int x = xStart - 1; x <= xEnd + 1; x++)
+                {
+                    var current = matrix[y][x];
+                    if (!char.IsDigit(current) && current != '.')
+                    { return true; }
+                }
+            }
+            return false;
+        }
+
+        return sum;
+
+    }
+
+    /*
+        [TestMethod]
+        public void P2()
+        {
+            var rslt = Calc2($"_{nameof(D03)}_1_sample.txt");
+            Assert.AreEqual(2286, rslt);
+
+            rslt = Calc2($"_{nameof(D03)}.txt");
+            Assert.AreEqual(78669, rslt);
+        }
+    */
+}
