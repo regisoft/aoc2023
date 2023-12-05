58f70911bbcbb72c494ac10bec5d9c1f60ce1143
ivo <ivo.marti@edorex.ch>
2023-12-05 08:33:55 +0100

    D04_2

--- D04.cs
+++ D04.cs
@@ -17,59 +17,72 @@ public class D04
 
     public static double Calc1(string inputName)
     {
-        var rslt = File.ReadLines(inputName).Select(i =>
+        return File.ReadLines(inputName).Select(i =>
         {
-            var x = i.Split(':')[1].Replace("  "," ").Split(" | ");
+            var x = i.Split(':')[1].Replace("  ", " ").Split(" | ");
             return new
             {
                 wins = x[0].Trim().Split(" "),
                 my = x[1].Trim().Split(" ")
             };
         })
-        /*
         .Select(game =>
         {
             var cnt = game.my.Intersect(game.wins).Count();
-            var score = cnt > 1 ? Math.Pow(2, cnt - 1) : cnt;
-
-            return new
-            {
-                game.wins,
-                game.my,
-                cnt,
-                score
-            };
-        }).ToList();
-        */
-        .Select(game =>
-        {
-            var cnt = game.my.Intersect(game.wins).Count();
-            if (cnt > 1) return Math.Pow(2, cnt - 1);
-            else return cnt;
+            return cnt > 1 ? Math.Pow(2, cnt - 1) : cnt;
         })
         .Sum();
-
-        //var dump = JsonSerializer.Serialize(rslt);
-        // create a file from dump
-        //File.WriteAllText("c:/tmp/dump.json", dump);   
-
-
-        //return 13;
-
-        return rslt;
     }
 
 
     [TestMethod]
     public void P2()
     {
-        /*
-               var rslt = Calc2($"input/_{nameof(D04)}_1_sample.txt");
-               Assert.AreEqual(467835, rslt);
+        var rslt = Calc2($"input/_{nameof(D04)}_1_sample.txt");
+        Assert.AreEqual(30, rslt);
 
-               rslt = Calc2($"input/_{nameof(D04)}.txt");
-               Assert.AreEqual(87287096, rslt);
-               */
+        rslt = Calc2($"input/_{nameof(D04)}.txt");
+        Assert.AreEqual(5704953, rslt);
     }
 
+    public static double Calc2(string inputName)
+    {
+        var rslt = File.ReadLines(inputName).Select(i =>
+        {
+            var x = i.Split(':')[1].Replace("  ", " ").Split(" | ");
+            return new
+            {
+                wins = x[0].Trim().Split(" "),
+                my = x[1].Trim().Split(" ")
+            };
+        })
+        .Select(game =>
+        {
+            var cnt = game.my.Intersect(game.wins).Count();
+
+            return new Card
+            {
+                Score = cnt,
+                Count = 1
+            };
+        })
+        .ToArray();
+
+        for (int cardIdx = 0; cardIdx < rslt.Length - 1; cardIdx++)
+        {
+            var copyNextCnt = rslt[cardIdx].Score;
+            for (int i = 0; i < copyNextCnt; i++)
+            {
+                rslt[cardIdx + 1 + i].Count += rslt[cardIdx].Count;
+            }
+        }
+
+        return rslt.Sum(c => c.Count);
+    }
+}
+
+public class Card
+{
+    public int Score;
+    public int Count;
 }
